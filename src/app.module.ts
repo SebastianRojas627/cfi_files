0da866dc448a213ae83358fbc53d787e3ce9ff1a
Sebastian Rojas <sebosinaga@gmail.com>
2025-04-25 15:51:38 -0400

    changed the response body of succesful upload

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,7 +21,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       host: process.env.DB_HOST,
       port: parseInt(process.env.DB_PORT || '5432', 10),
       username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
+      password: String(process.env.DB_PASSWORD),
       database: process.env.DB_NAME,
       autoLoadEntities: true,
       synchronize: true,
